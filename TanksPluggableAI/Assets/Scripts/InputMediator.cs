b4603cb332c1b1bc239d65342ea3978f92807991
harhal19 <harhal19@mail.ru>
2018-12-26 02:52:32 +0300

    Added input mediator

---
+++ TanksPluggableAI/Assets/Scripts/InputMediator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMediator : MonoBehaviour
+{
+
+	private static InputMediator _commonInputMediator;
+
+	public static Vector2 GetMovementInput()
+	{
+		return _commonInputMediator.MovementInput;
+	}
+
+	public static bool GetFireInput()
+	{
+		return _commonInputMediator.FireInput;
+	}
+	
+	private Vector2 MovementInput;
+	private bool FireInput;
+
+	[SerializeField] private bool UseKeyboardInput = true;
+	
+	// Use this for initialization
+	void Awake ()
+	{
+		_commonInputMediator = this;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (UseKeyboardInput)
+		{
+			MovementInput = new Vector2(Input.GetAxis("Horizontal1"), Input.GetAxis("Vertical1"));
+			FireInput = Input.GetAxis("Fire1") > 0.5f;
+		}
+	}
+
+	public void SetMovementInput(Vector2 Input)
+	{
+		MovementInput = Input;
+	}
+
+	public void SetFireInput(bool Input)
+	{
+		FireInput = Input;
+	}
+}
